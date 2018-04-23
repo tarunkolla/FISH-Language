f9f496f3f799620877bcdf1535c8f8247012c743
Koushik Kotamraju <kkotamra@asu.edu>
2018-04-23 10:03:23 -0700

    Setting Main to take Arguments

--- src/compiler/FishMain.java
+++ src/compiler/FishMain.java
@@ -12,7 +12,7 @@ class FishMain{
 	@SuppressWarnings("deprecation")
 	public static void main(String args[]) throws Exception
 	{
-		FileInputStream fis = new FileInputStream("C:\\users\\koush\\Desktop\\firstFish.fish");
+		FileInputStream fis = new FileInputStream(args[0]);
 		ANTLRInputStream input = new ANTLRInputStream(fis);
 		FishLanguageLexer lexer = new FishLanguageLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -23,7 +23,7 @@ class FishMain{
 		ptw.walk(flb, tree);
 		try
         {
-        	PrintWriter writer = new PrintWriter("firstFish" + ".ic", "UTF-8");
+        	PrintWriter writer = new PrintWriter(args[0] + ".ic", "UTF-8");
         	for(int i=0;i<flb.instructionStack.size();i++)
         	{
         		writer.println(flb.instructionStack.get(i));
@@ -36,7 +36,7 @@ class FishMain{
         	e.printStackTrace();
         }
 		FishRunTimeMain runTime = new FishRunTimeMain();
-		runTime.start("firstFish"+".ic");
+		runTime.start(args[0]+".ic");
 		
 	}
 }
